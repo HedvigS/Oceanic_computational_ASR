6e67287950e22694ea4f704b1a4444c238daed2b
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-05-13 17:25:15 +0200

    Update run_entire_project.sh

--- code/run_entire_project.sh
+++ code/run_entire_project.sh
@@ -1,25 +1,27 @@
 #1w:code skirgard$ chmod 755 run_entire_project.sh 
 #lingn01w:code skirgard$ ./run_entire_project.sh 
 
+#Step 1
+echo first step, installing necessary packages
 Rscript 1_requirements.R 
 #python3 -m pip install -r 1_requirements.txt
 
+#Step 2
 echo second step, fecthing data
 Rscript 2_get_glottolog_language_table.R
 Rscript 2_get_grambank_data.R 
 
 ## 3. prep trees
+echo third step, prep trees
 
 #	python3 3_create_tree_bottom_up.py #will create a tree of Oceanic languages where the tips are languoids in grambank
-#echo "Extracted Oceanic subgroup from glottolog tree and pruned so that all tips are languoids in Grambank."
-	
 Rscript 3_get_gray_tree.R
 	
 ## 4 run ASR anlsysis -Parsimony
 # These scripts do the ASR and deposits the results in an Rdata file and makes a tree-plot for each feature and method and outputs a PNG file.
 #step_4: max_parsimony
 
-#max_parsimony:
+echo fourth step, run the max parsimony ancestral state reconstruction analysis
 	Rscript 4_ASR_parsimony_glottolog.R
 #	Rscript 4_ASR_parsimony_gray.R
 
@@ -27,14 +29,15 @@ Rscript 3_get_gray_tree.R
 # These scripts do the ASR and deposits the results in an Rdata file and makes a tree-plot for each feature and method and outputs a PNG file.
 #step_5: max_likelihood
 
+echo fifth step, run the max liklihood (marginal) ancestral state reconstruction analysis
+
 #max_likelihood:
-#	Rscript 5_ASR_ML_glottolog.R
-#	Rscript 5_ASR_ML_gray.R
+	Rscript 5_ASR_ML_glottolog.R
+	Rscript 5_ASR_ML_gray.R
 
 ## 6 run ASR analysis - SIMMAP
 
 ## 7 Compare to HL
-#step_7: summary_tables
+echo seventh step, compate to classical historical linguistics
 
-#summary_tables:
-#	Rscript 7_compare_to_HL.R
+Rscript 7_compare_to_HL.R
