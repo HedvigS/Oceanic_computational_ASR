632f68ce0bf7da4c991faa5e3c3ce4688db2c88c
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-07-21 11:50:52 +0200

    Update 7b_get_ancestral_states_ML_gray_posteriors.R

--- code/analysis_scripts_gray_all_posterior/7b_get_ancestral_states_ML_gray_posteriors.R
+++ code/analysis_scripts_gray_all_posterior/7b_get_ancestral_states_ML_gray_posteriors.R
@@ -80,7 +80,7 @@ glottolog_df <- read_tsv("data/glottolog_language_table_wide_df.tsv", col_types 
 GB_df_desc <- read_tsv("data/GB/parameters_binary.tsv") %>% 
   dplyr::select(Feature_ID = ID, Abbreviation =Grambank_ID_desc, Question = Name) 
 #function for getting node positions over ML objects
-get_node_positions <- function(GB_asr_object_ml){
+get_node_positions_ML <- function(GB_asr_object_ml){
   
   #GB_asr_object_ml <- GB_ASR_RDS_ML_gray$content[[1]][[1]]
   
@@ -156,52 +156,50 @@ for(dir in dirs){
   
   value_count_df <- read_csv(file.path(dir, "results.csv"), col_types = 
                                cols(
-                                 Parsimony_cost = col_double(),
-                                 `0` = col_double(),
-                                 `1` = col_double(),
                                  Feature_ID = col_character(),
-                                 ntips = col_double()
-                               )) %>% 
-    mutate(min = pmin(`0`, `1`)) %>% 
-    mutate(min_percent_parsimony_gray = min / (`0`+ `1`)) %>%
-    dplyr::select(Feature_ID, ntips_parsimony_gray = ntips, zeroes_parsimony_gray = `0`, ones_parsimony_gray = `1`, min_percent_parsimony_gray)
+                                 LogLikelihood = col_double(),
+                                 AICc = col_double(),
+                                 pRoot0 = col_double(),
+                                 pRoot1 = col_double(),
+                                 q01 = col_double(),
+                                 q10 = col_double(),
+                                 nTips = col_double(),
+                                 nTips_state_0 = col_double(),
+                                 nTips_state_1 = col_double()                               )) %>% 
+    mutate(min = pmin(`nTips_state_0`, `nTips_state_1`)) %>% 
+    mutate(min_percent_ML_gray = min / (`nTips_state_0`+ `nTips_state_1`)) %>%
+    dplyr::select(Feature_ID, ntips_ML_gray = nTips, zeroes_ML_gray = `nTips_state_0`, ones_ML_gray = `nTips_state_1`, min_percent_ML_gray)
   
+  ###Gray ML
+  GB_ACR_all_ML <- readRDS(file.path(dir, "GB_ML_gray_tree.rds"))
   
+  df_lik_anc_ML_gray <- lapply(GB_ACR_all_ML$content, get_node_positions_ML) %>% bind_rows()
+    
+  df_lik_anc_ML_gray$gray_ML_prediction <- if_else(df_lik_anc_ML_gray$`0` > 0.6, "Absent", if_else(df_lik_anc_ML_gray$`1` > 0.6, "Present", "Half")) 
   
-  ###Gray parsimony
-  GB_ACR_all_parsimony <- readRDS(file.path(dir, "GB_parsimony_gray_tree.rds"))
-  
-  df_lik_anc_parsimony_gray <- lapply(GB_ACR_all_parsimony$content, get_node_positions) %>% bind_rows()
-  
-  df_lik_anc_parsimony_gray$gray_parsimony_prediction <- if_else(df_lik_anc_parsimony_gray$`0` > 0.6, "Absent", if_else(df_lik_anc_parsimony_gray$`1` > 0.6, "Present", "Half")) 
-  
-  df_lik_anc_parsimony_gray <- df_lik_anc_parsimony_gray %>% 
+  df_lik_anc_ML_gray <- df_lik_anc_ML_gray %>% 
     mutate(`0` = round(`0`)) %>% 
     mutate(`1` = round(`1`)) %>% 
-    dplyr::select(Feature_ID, "Proto-language", gray_parsimony_prediction,gray_parsimony_prediction_0 = `0`, gray_parsimony_prediction_1 = `1`)
+    dplyr::select(Feature_ID, "Proto-language", gray_ML_prediction,gray_ML_prediction_0 = `0`, gray_ML_prediction_1 = `1`)
   
   df <- HL_findings_sheet %>% 
-    right_join(df_lik_anc_parsimony_gray, by = c("Feature_ID", "Proto-language")) 
+    right_join(df_lik_anc_ML_gray, by = c("Feature_ID", "Proto-language")) 
   
+    df$`ML result (Gray et al 2009-tree)` <- ifelse(df$gray_ML_prediction == "Present" & df$Prediction == 1, "True Positive",  
+                                                          ifelse(df$gray_ML_prediction == "Absent" & df$Prediction == 0, "True Negative",   
+                                                                  ifelse(df$gray_ML_prediction == "Absent" & df$Prediction == 1, "False Negative",  
+                                                                          ifelse(df$gray_ML_prediction == "Present" & df$Prediction == 0, "False Positive",           ifelse(df$gray_ML_prediction == "Half", "Half", NA)))))
   
-  df$`Parsimony result (Gray et al 2009-tree)` <- if_else(df$gray_parsimony_prediction == "Present" & df$Prediction == 1, "True Positive",  
-                                                          if_else(df$gray_parsimony_prediction == "Absent" & df$Prediction == 0, "True Negative",   
-                                                                  if_else(df$gray_parsimony_prediction == "Absent" & df$Prediction == 1, "False Negative",  
-                                                                          if_else(df$gray_parsimony_prediction == "Present" & df$Prediction == 0, "False Positive",
-                                                                                  
-                                                                                  ifelse(df$gray_parsimony_prediction == "Half", "Half", NA)))))
-  
-  
-  df <- value_count_df %>% 
+    df <- value_count_df %>% 
     right_join(df, by = "Feature_ID") 
-  
+
   ##Marking which results can't be included because they don't have enough languages
   
-  #Parsimony gray
-  df$`Parsimony result (Gray et al 2009-tree)` <- if_else(df$ntips_parsimony_gray <  ntips_half_gray, "Not enough languages", df$`Parsimony result (Gray et al 2009-tree)`)
-  df$gray_parsimony_prediction <- if_else(df$ntips_parsimony_gray <  ntips_half_gray, "Not enough languages", df$gray_parsimony_prediction)
-  df$gray_parsimony_prediction_1 <- ifelse(df$ntips_parsimony_gray <  ntips_half_gray, NA, df$gray_parsimony_prediction_1)
-  df$gray_parsimony_prediction_0 <- ifelse(df$ntips_parsimony_gray <  ntips_half_gray, NA, df$gray_parsimony_prediction_0)
+  #ML gray
+  df$gray_ML_prediction <- if_else(df$ntips_ML_gray <  ntips_half_gray, "Not enough languages", df$gray_ML_prediction)
+  df$gray_ML_prediction_1 <- ifelse(df$ntips_ML_gray <  ntips_half_gray, NA, df$gray_ML_prediction_1)
+  
+  df$gray_ML_prediction_0 <- ifelse(df$ntips_ML_gray <  ntips_half_gray, NA, df$gray_ML_prediction_0)
   
   #Counting up trues per feature
   df$countTruePos <- rowSums(df == "True Positive", na.rm = T)
