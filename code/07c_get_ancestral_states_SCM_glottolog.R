7e4fc06e6eab0c367ef05215c354a013e8f81fec
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-09-22 17:57:30 +0200

    Update 07c_get_ancestral_states_SCM_glottolog.R

--- code/07c_get_ancestral_states_SCM_glottolog.R
+++ code/07c_get_ancestral_states_SCM_glottolog.R
@@ -3,43 +3,42 @@ source("01_requirements.R")
 #reading in old sheet with HL-predictions
 #the reason for reading them in like this instead of subsetting the GB_wide table is because I'd like to use the LaTeX source formatting which exists in an extra col in the raw sheets
 
-HL_findings_sheet <-  read_tsv("data/HL_findings/HL_findings_for_comparison.tsv")
+HL_findings_sheet <- read_tsv("data/HL_findings/HL_findings_for_comparison.tsv")
 
+HL_findings_sheet_conflicts <- read_csv("data/HL_findings/HL_findings_conflicts.csv") %>% 
+  mutate(conflict = "Yes") %>% 
+  rename(Prediction = Value)
 
-##creating dfs which show the number of tips per tree per method, as well as the general distribution at the tips. This makes it possible for us for example to exclude results with too few tips. We'll use this df later to filter with
+HL_findings_sheets <- HL_findings_sheet %>% 
+  full_join(HL_findings_sheet_conflicts)
 
-
-#value_count_df <- read_csv("output/glottolog_tree_binary/SCM/
-#  mutate(min = pmin( nTips_state_0,  nTips_state_1)) %>% 
-#  mutate(min_percent_SCM_gray = min / nTips) %>%
-#  dplyr::select(Feature_ID, ntips_SCM_gray = nTips, zeroes_SCM_gray =  nTips_state_0, ones_SCM_gray =  nTips_state_1, min_percent_SCM_gray)
-
-
-  
-value_count_df <- value_counts_ML_gray %>% 
-  full_join(value_counts_ML_glottolog) %>%
-  full_join(value_counts_parsimony_glottolog) %>%
-  full_join(value_counts_parsimony_gray) %>% 
-  full_join(value_counts_SCM_gray)
-
-rm(value_counts_ML_gray, value_counts_ML_glottolog, value_counts_parsimony_glottolog, value_counts_parsimony_gray)
+value_count_df <-read_csv("output/glottolog_tree_binary/SCM/results.csv") %>% 
+  mutate(min = pmin( nTips_state_0,  nTips_state_1)) %>% 
+  mutate(min_percent_SCM_glottolog = min / nTips) %>%
+  dplyr::select(Feature_ID, ntips_SCM_glottolog = nTips, zeroes_SCM_glottolog =  nTips_state_0, ones_SCM_glottolog =  nTips_state_1, min_percent_SCM_glottolog)
 
 #glottolog df information with branch names, so that we can easily subset for the different groups based on "classification"
 #reading in glottolog language table (to be used for language names for plot and to pre-filter out non-oceanic
 glottolog_df <- read_tsv("data/glottolog_language_table_wide_df.tsv", col_types = cols())  %>% 
   dplyr::select(Glottocode, classification, Name)
 
-##########################################  ML ##########################################
+##glottolog scm object
+GB_ASR_RDS_SCM_glottolog <- readRDS("output/glottolog_tree_binary/SCM/GB_SCM_glottolog_tree.rds") %>% 
+  left_join(value_count_df, by = "Feature_ID") %>% 
+  filter(!is.na(ntips_SCM_glottolog))
 
-#Function for getting ancestral states for 4 specific nodes out of corHMM gray version
-get_node_positions_ML <- function(GB_asr_object_ml){
+##########################################  SCM function ##########################################
 
-#GB_asr_object_ml <- GB_ASR_RDS_ML_gray$content[[1]]
+#Function for getting ancestral states for 4 specific nodes out of scm glottolog
+get_node_positions_SCM <- function(GB_asr_object_SCM){
 
-GB_asr_object_ml <- GB_asr_object_ml[[1]]
+#GB_asr_object_SCM <- GB_ASR_RDS_SCM_glottolog$content[[1]]
 
-feature <- GB_asr_object_ml$data %>% colnames() %>% .[2]
-tree <- GB_asr_object_ml$phy
+GB_asr_object_scm_SIMMAP_summary <- GB_asr_object_SCM[[1]]
+GB_asr_object_scm_results_df <- GB_asr_object_SCM[[2]]
+
+feature <- GB_asr_object_scm_results_df$Feature_ID
+tree <- GB_asr_object_scm_SIMMAP_summary$tree
 
 tip_label_df <- tree$tip.label %>% 
   as.data.frame() %>% 
@@ -94,10 +93,6 @@ cat("I'm done with finding the ML proto-language states for feature ", feature, 
 df
 }
 
-##Gray tree ML 
-GB_ASR_RDS_ML_gray <- readRDS("output/gray_et_al_2009/ML/mcct/GB_ML_gray_tree.rds") %>% 
-  left_join(value_count_df, by = "Feature_ID") %>% 
-  filter(!is.na(ntips_ML_gray))
 
 df_lik_anc_ML_gray <- lapply(GB_ASR_RDS_ML_gray$content, get_node_positions_ML) %>% bind_rows()
 
