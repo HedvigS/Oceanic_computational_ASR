8b33cf582e3a7bbabb41089bd92ea3d0bcaf2d51
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-09-28 10:29:40 +0200

    Update run_entire_project_mcct.sh

--- code/run_entire_project_mcct.sh
+++ code/run_entire_project_mcct.sh
@@ -57,19 +57,31 @@ Rscript 04_ASR_parsimony_glottolog.R
 # These scripts do the ASR and deposits the results in an Rdata file and makes a tree-plot for each feature and method and outputs a PNG file.
 #step_5: max_likelihood
 
-echo "fifth step, run the max liklihood (marginal) ancestral state reconstruction analysis  on the glottolog tree and the MCCT tree from Gray et al 2009.""
-Rscript analysis_scripts_gray_mcct/05_ASR_ML_gray_mcct""
+echo "fifth step, run the max liklihood (marginal) ancestral state reconstruction analysis  on the glottolog tree and the MCCT tree from Gray et al 2009."
+Rscript analysis_scripts_gray_mcct/05_ASR_ML_gray_mcct.R
 Rscript 05_ASR_ML_glottolog.R
 
 ## 6 run ASR analysis - SIMMAP
+echo "fifth step, run the SCM reconstruction analysis  on the glottolog tree and the MCCT tree from Gray et al 2009."
+Rscript analysis_scripts_gray_mcct/06_ASR_SCM_gray_mcct.R
+Rscript 06_ASR_SCM_glottolog.R
 
 ## 7 get all the states for 4 proto-languages
-echo sevent step, get ancestral states
+echo "seventh step, get ancestral states"
 
-Rscript analysis_scripts_gray_mcct/07_get_ancestral_states.R
+echo "Glottolog tree"
+Rscript 07a_get_ancestral_states_parsimony_glottolog.R
+Rscript 07b_get_ancestral_states_ML_glottolog.R
+Rscript 07c_get_ancestral_states_SCM_glottolog.R
+
+echo "Gray et al tree (MCCT)"
+Rscript 07a_get_ancestral_states_parsimony_mcct.R
+Rscrpt 07b_get_ancestral_states_ML_mcct.R
+Rscrpt 07c_get_ancestral_states_SCM_mcct.R
+
 
 ## 8 compare to HL
 
-echo eigth step, compate to classical historical linguistics
+#echo eigth step, compate to classical historical linguistics
 
-Rscript analysis_scripts_gray_mcct/08_compare_to_HL.R
+#Rscript analysis_scripts_gray_mcct/08_compare_to_HL.R
