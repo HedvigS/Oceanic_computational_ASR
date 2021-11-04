ac84e4fa1aa92ea8fc8c7340a6e21c0b70394002
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-11-04 22:46:29 +0100

    Update 07d_get_ancestral_states_most_common.R

--- code/07d_get_ancestral_states_most_common.R
+++ code/07d_get_ancestral_states_most_common.R
@@ -14,7 +14,7 @@ east_polynesian_df <- GB_df_long %>%
   mutate(value = str_replace_all(value, "1", "Present")) %>% 
   mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
-  summarise(n = n()) %>% 
+  dplyr::summarise(n = n()) %>% 
   dplyr::top_n(1, wt = n) %>% 
   mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
   mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
@@ -29,7 +29,7 @@ polynesian_df <- GB_df_long %>%
   mutate(value = str_replace_all(value, "1", "Present")) %>% 
   mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
-  summarise(n = n()) %>% 
+  dplyr::summarise(n = n()) %>% 
   dplyr::top_n(1, wt = n) %>% 
   mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
   mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
@@ -43,7 +43,7 @@ central_pacific_df <- GB_df_long %>%
   mutate(value = str_replace_all(value, "1", "Present")) %>% 
   mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
-  summarise(n = n()) %>% 
+  dplyr::summarise(n = n()) %>% 
   dplyr::top_n(1, wt = n) %>% 
   mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
   mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
@@ -59,7 +59,7 @@ oceanic_df <- GB_df_long %>%
   mutate(value = str_replace_all(value, "1", "Present")) %>% 
   mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
-  summarise(n = n()) %>% 
+  dplyr::summarise(n = n()) %>% 
   dplyr::top_n(1, wt = n) %>% 
   mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
   mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
@@ -67,39 +67,28 @@ oceanic_df <- GB_df_long %>%
   mutate(`Proto-language` = "Proto-Oceanic")
 
 
-  
+all_df <- oceanic_df  %>% 
+  full_join(central_pacific_df) %>% 
+  full_join(polynesian_df) %>% 
+  full_join(east_polynesian_df) %>% 
+  dplyr::rename(Feature_ID = variable)
 
-
-
-
-
-
-
-
-
-
-
-#First we make a df with the basic counts for all oceanic languages per feature and what the distribution at the tips are. This will help us identify cases where all tips where of the same kind. These cases are currently excluded from the ML workflow since corHMM doesn't accept trees where the values are the same at all tips. The parsimony function however accepts such cases, so in order to make them comparable we need to put them back in to the results. We can use the basic summary table from the parsimony glottolog analysis for this since that per definition includes all lgs. Note that the total distribution over all lgs isn't the same as the distribution in the gray tree, which may be missing specific tips which have the divergent value.
-values_df <- read_tsv("output/glottolog_tree_binary/parsimony/all_reconstructions.tsv") %>% 
-  distinct(Feature_ID, ntips = ntips_parsimony_glottolog, zeroes_total= zeroes_parsimony_glottolog ,ones_total=  ones_parsimony_glottolog, min_percent = min_percent_parsimony_glottolog)
 
 HL_findings_sheet <- read_tsv("data/HL_findings/HL_findings_for_comparison.tsv") %>% 
   filter(!is.na(Prediction)) %>% 
   dplyr::select(Feature_ID, Prediction, `Proto-language`)
 
-values_df <- values_df %>% 
+values_df <- all_df %>% 
   right_join(HL_findings_sheet) %>% 
-  distinct(Feature_ID, zeroes_total, ones_total, `Proto-language`, Prediction)
+  distinct(Feature_ID,`Proto-language`, Prediction, value)
 
-values_df$most_common_value <- ifelse(values_df$zeroes_total > values_df$ones_total, 0, 1)
-
-values_df$result_most_common <- ifelse(values_df$most_common_value == 1 & values_df$Prediction == 1, "True Positive",  
-                                       ifelse(values_df$most_common_value == 0 & values_df$Prediction == 0, "True Negative",   
-                                              ifelse(values_df$most_common_value == 0 & values_df$Prediction == 1, "False Negative",  
-                                                     ifelse(values_df$most_common_value == 1 & values_df$Prediction == 0, "False Positive",
+values_df$result_most_common <- ifelse(values_df$value == "Present" & values_df$Prediction == 1, "True Positive",  
+                                       ifelse(values_df$value == "Absent" & values_df$Prediction == 0, "True Negative",   
+                                              ifelse(values_df$value == "Absent" & values_df$Prediction == 1, "False Negative",  
+                                                     ifelse(values_df$value == "Present" & values_df$Prediction == 0, "False Positive",
                                                             
-                                                            ifelse(values_df$most_common_value == "Half", "Half", NA)))))
-most_common_df <- values_df %>% 
-  dplyr::rename(variable = result_most_common) %>% 
-  group_by(variable) %>% 
-  summarise(Most_common_value = n())
+                                                            ifelse(values_df$value == "Half", "Half", NA)))))
+all_df  %>% 
+  left_join(values_df) %>% 
+  dplyr::select(-Prediction) %>% 
+  write_tsv("output/HL_comparison/most_common_reconstructions.tsv")
