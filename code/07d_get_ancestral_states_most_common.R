c57c5b5ad3551e59c65e739bcda9ca5b102c0074
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-11-30 21:47:26 +0100

    Update 07d_get_ancestral_states_most_common.R

--- code/07d_get_ancestral_states_most_common.R
+++ code/07d_get_ancestral_states_most_common.R
@@ -17,68 +17,75 @@ east_polynesian_df <- GB_df_long %>%
   left_join(glottolog_df) %>% 
   filter(str_detect(classification, "east2449")) %>% 
   mutate(value = as.character(value)) %>% 
-  mutate(value = str_replace_all(value, "1", "Present")) %>% 
-  mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
   dplyr::summarise(n = n()) %>% 
-  dplyr::top_n(1, wt = n) %>% 
-  mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% #if there are equal amounts that are "top", top_n() will keep both. All of these are per definition states where there are the same amount of languages that are 1 and 0. 
-  mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
-  distinct(variable, value) %>% 
+  ungroup() %>% 
+  complete(variable, value, fill = list(n = 0)) %>% 
+  group_by(variable) %>% 
+  mutate(all = sum(n, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(percentage = n / all) %>% 
+  reshape2::dcast(variable~value, value.var = "percentage") %>% 
   mutate(`Proto-language` = "Proto-Eastern Polynesian")
 
+east_polynesian_df$most_common_prediction <- if_else(east_polynesian_df$`0` > 0.6, "Absent", if_else(east_polynesian_df$`1` > 0.6, "Present", "Half")) 
 
 polynesian_df <- GB_df_long %>% 
   left_join(glottolog_df) %>% 
   filter(str_detect(classification, "poly1242")) %>%
   mutate(value = as.character(value)) %>% 
-  mutate(value = str_replace_all(value, "1", "Present")) %>% 
-  mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
   dplyr::summarise(n = n()) %>% 
-  dplyr::top_n(1, wt = n) %>% 
-  mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
-  mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
-  distinct(variable, value) %>% 
+  ungroup() %>% 
+  complete(variable, value, fill = list(n = 0)) %>% 
+  group_by(variable) %>% 
+  mutate(all = sum(n, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(percentage = n / all) %>% 
+  reshape2::dcast(variable~value, value.var = "percentage") %>% 
   mutate(`Proto-language` = "Proto-Polynesian")
+
+polynesian_df$most_common_prediction <- if_else(polynesian_df$`0` > 0.6, "Absent", if_else(polynesian_df$`1` > 0.6, "Present", "Half")) 
 
 central_pacific_df <- GB_df_long %>% 
   left_join(glottolog_df) %>% 
   filter(str_detect(classification, "cent2060")) %>% 
   mutate(value = as.character(value)) %>% 
-  mutate(value = str_replace_all(value, "1", "Present")) %>% 
-  mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
   dplyr::summarise(n = n()) %>% 
-  dplyr::top_n(1, wt = n) %>% 
-  mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
-  mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
-  distinct(variable, value) %>% 
+  ungroup() %>% 
+  complete(variable, value, fill = list(n = 0)) %>% 
+  group_by(variable) %>% 
+  mutate(all = sum(n, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(percentage = n / all) %>% 
+  reshape2::dcast(variable~value, value.var = "percentage") %>% 
   mutate(`Proto-language` = "Proto-Central Pacific")
 
-
+central_pacific_df$most_common_prediction <- if_else(central_pacific_df$`0` > 0.6, "Absent", if_else(central_pacific_df$`1` > 0.6, "Present", "Half")) 
 
 oceanic_df <- GB_df_long %>% 
   left_join(glottolog_df) %>% 
   filter(str_detect(classification, "ocea1241")) %>% 
   mutate(value = as.character(value)) %>% 
-  mutate(value = str_replace_all(value, "1", "Present")) %>% 
-  mutate(value = str_replace_all(value, "0", "Absent")) %>% 
   group_by(variable, value) %>% 
   dplyr::summarise(n = n()) %>% 
-  dplyr::top_n(1, wt = n) %>% 
-  mutate(dup = duplicated(variable) + duplicated(variable, fromLast = T)) %>% 
-  mutate(value = ifelse(dup >= 1,"Half", value )) %>% 
-  distinct(variable, value) %>% 
+  ungroup() %>% 
+  complete(variable, value, fill = list(n = 0)) %>% 
+  group_by(variable) %>% 
+  mutate(all = sum(n, na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(percentage = n / all) %>% 
+  reshape2::dcast(variable~value, value.var = "percentage") %>% 
   mutate(`Proto-language` = "Proto-Oceanic")
 
+oceanic_df$most_common_prediction <- if_else(oceanic_df$`0` > 0.6, "Absent", if_else(oceanic_df$`1` > 0.6, "Present", "Half")) 
 
 all_df <- oceanic_df  %>% 
   full_join(central_pacific_df) %>% 
   full_join(polynesian_df) %>% 
   full_join(east_polynesian_df) %>% 
   dplyr::rename(Feature_ID = variable)
-
 
 HL_findings_sheet <- read_tsv("data/HL_findings/HL_findings_for_comparison.tsv") %>% 
   full_join(read_csv("data/HL_findings/HL_findings_conflicts.csv") %>% 
@@ -88,19 +95,19 @@ HL_findings_sheet <- read_tsv("data/HL_findings/HL_findings_for_comparison.tsv")
 
 values_df <- all_df %>% 
   right_join(HL_findings_sheet) %>% 
-  distinct(Feature_ID,`Proto-language`, Prediction, value)
+  distinct(Feature_ID,`Proto-language`, Prediction, most_common_prediction)
 
-values_df$result_most_common <- ifelse(values_df$value == "Present" & values_df$Prediction == 1, "True Positive",  
-                                       ifelse(values_df$value == "Absent" & values_df$Prediction == 0, "True Negative",   
-                                              ifelse(values_df$value == "Absent" & values_df$Prediction == 1, "False Negative",  
-                                                     ifelse(values_df$value == "Present" & values_df$Prediction == 0, "False Positive",
+values_df$result_most_common <- ifelse(values_df$most_common_prediction == "Present" & values_df$Prediction == 1, "True Positive",  
+                                       ifelse(values_df$most_common_prediction == "Absent" & values_df$Prediction == 0, "True Negative",   
+                                              ifelse(values_df$most_common_prediction == "Absent" & values_df$Prediction == 1, "False Negative",  
+                                                     ifelse(values_df$most_common_prediction == "Present" & values_df$Prediction == 0, "False Positive",
                                                             
-                                                            ifelse(values_df$value == "Half", "Half", NA)))))
+                                                            ifelse(values_df$most_common_prediction == "Half", "Half", NA)))))
 
 all_df  %>% 
   left_join(values_df) %>% 
   left_join(not_enough_languages_df) %>% 
-  mutate(value = ifelse(not_enough == "Not enough languages", "Not enough languages", value)) %>% 
+  mutate(most_common_prediction = ifelse(not_enough == "Not enough languages", "Not enough languages", most_common_prediction)) %>% 
   mutate(result_most_common = ifelse(not_enough == "Not enough languages", "Not enough languages", result_most_common)) %>% 
-  dplyr::select(-not_enough, -n) %>%
+  dplyr::select(-not_enough, -n, -Prediction) %>% 
   write_tsv("output/HL_comparison/most_common_reconstructions.tsv")
