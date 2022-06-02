d6eaabd9ce19841f7359ff5f449f9f787b001b35
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-06-02 19:11:38 +0200

    Update 03_densitree.R

--- code/03_densitree.R
+++ code/03_densitree.R
@@ -1,20 +1,10 @@
 source("01_requirements.R")
-
-if(!file.exists("data/trees/gray_et_al_2009_posterior_trees_pruned/gray_et_al_2009_posterior_tree_pruned_1.txt")){
-  source("analysis_scripts_gray_all_posterior/03_process_gray_tree_posterios.R")}
-
-fns <- list.files("data/trees/gray_et_al_2009_posterior_trees_pruned/", full.names = T)
-
 h_load("phangorn")
 
-trees <- read.tree(fns[1])
+if(!file.exists(file.path("data", "trees", "posterios_pruned_multiPhylo.txt"))){
+  source("analysis_scripts_gray_all_posterior/03_process_gray_tree_posterios.R")}
 
-for(fn in fns[-1]){
-#  fn <- fns[1]
-tree <- read.tree(fn)
-trees <-   c(trees, tree)
-}
-
+trees <- read.tree(file = file.path("data", "trees", "posterios_pruned_multiPhylo.txt"))
 
 png("output/coverage_plots/tree/gray_et_al_2009_100_sample_densitree.png", width = 10.69, height = 10.69, units = "in", res = 600)
 phangorn::densiTree(trees, col="chartreuse3",  tip.color = "white", scale.bar = F)
