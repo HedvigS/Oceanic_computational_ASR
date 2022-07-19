2d42af21a84d5038160cd2cae498f46620908ef2
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-19 18:10:49 +0200

    Update 09e_extra_predictions.R

--- code/09e_extra_predictions.R
+++ code/09e_extra_predictions.R
@@ -3,6 +3,11 @@ source("01_requirements.R")
 GB_ID_desc <- read_tsv("../grambank-analysed/R_grambank/output/GB_wide/parameters_binary.tsv") %>% 
   dplyr::select(Feature_ID = ID, Name)
 
+#OUTPUTTING XTABLES
+OUTPUT_DIR <- file.path( OUTPUTDIR_plots , "results")
+if(!dir.exists(OUTPUT_DIR)){
+  dir.create(OUTPUT_DIR)}
+
 #HL sheets
 HL_findings_sheet <- read_tsv("output/processed_data/HL_findings/HL_findings_for_comparison.tsv") %>% 
   filter(!is.na(Prediction)) %>% 
@@ -53,11 +58,9 @@ full_df <- parsimony_glottolog_df %>%
   full_join(ML_gray_posteriors_df) %>% 
   full_join(most_common_df)
 
-
-
 full_df %>% 
   reshape2::melt(id.vars = c("Feature_ID", "Proto-language")) %>% 
   group_by(Feature_ID, `Proto-language`, value) %>% 
-  summarise(n = n()) %>% View()
-  
-  
+  summarise(n = n()) %>% 
+  left_join(GB_ID_desc) %>% 
+  write_tsv("output/HL_comparison/extra_predictions"))
