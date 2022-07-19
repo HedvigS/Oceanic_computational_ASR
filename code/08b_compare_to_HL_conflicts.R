fdbe3651e881dd1ebf037c77649584d95edd7479
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-19 14:11:09 +0200

    Update 08b_compare_to_HL_conflicts.R

--- code/08b_compare_to_HL_conflicts.R
+++ code/08b_compare_to_HL_conflicts.R
@@ -3,38 +3,38 @@ source("01_requirements.R")
 HL_findings_sheet <- read_csv(HL_findings_sheet_conflicts_fn) %>% 
   rename(Prediction = Value) %>% 
   filter(!is.na(Prediction)) %>% 
-  dplyr::select(Feature_ID, Prediction, `Proto-language`, Source, Feature)
+  dplyr::select(Feature_ID, HL_prediction = Prediction, `Proto-language`, Source, Feature)
 
 most_common_df <- read_tsv("output/HL_comparison/most_common_reconstructions.tsv") %>% 
   filter(!is.na(`result_most_common`)) %>% 
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, "most_common_prediction", "Proto-language")
+  dplyr::select(Feature_ID, "most_common_prediction", "Proto-language", HL_prediction)
 
 #reading in the results from each method and tree and calculating the number of true negatives etc
 
 parsimony_glottolog_df <- read_tsv("output/glottolog-tree/parsimony/all_reconstructions.tsv") %>% 
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, `Parsimony result (Glottolog-tree)`, "Proto-language")
+  dplyr::select(Feature_ID, `Parsimony result (Glottolog-tree)`, "Proto-language", HL_prediction)
 
 parsimony_gray_mcct_df <- read_tsv("output/gray_et_al_2009/parsimony/mcct/all_reconstructions.tsv") %>%
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, `Parsimony result (Gray et al 2009-tree)`, "Proto-language")
+  dplyr::select(Feature_ID, `Parsimony result (Gray et al 2009-tree)`, "Proto-language", HL_prediction)
 
 parsimon_gray_posteriors_df <- read_tsv("output/gray_et_al_2009/parsimony/all_reconstructions_posteriors_aggregated.tsv")   %>% 
   right_join(HL_findings_sheet) %>%
-  dplyr::select(Feature_ID, `Parsimony result (Gray et al 2009-tree posteriors)`, "Proto-language")
+  dplyr::select(Feature_ID, `Parsimony result (Gray et al 2009-tree posteriors)`, "Proto-language", HL_prediction)
 
 ML_glottolog_df <- read_tsv("output/glottolog-tree/ML/all_reconstructions.tsv") %>% 
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, `ML result (Glottolog-tree)`, "Proto-language")
+  dplyr::select(Feature_ID, `ML result (Glottolog-tree)`, "Proto-language", HL_prediction)
 
 ML_gray_mcct <- read_tsv("output/gray_et_al_2009/ML/mcct/all_reconstructions.tsv") %>% 
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, `ML result (Gray et al 2009-tree)`, "Proto-language")
+  dplyr::select(Feature_ID, `ML result (Gray et al 2009-tree)`, "Proto-language", HL_prediction)
 
 ML_gray_posteriors_df <- read_tsv("output/gray_et_al_2009/ML/all_reconstructions_posteriors_aggregated.tsv") %>% 
   right_join(HL_findings_sheet) %>% 
-  dplyr::select(Feature_ID, `ML result (Gray et al 2009-tree posteriors)`, "Proto-language")
+  dplyr::select(Feature_ID, `ML result (Gray et al 2009-tree posteriors)`, "Proto-language", HL_prediction)
 
   
 
