ec005150db629cecf10dfbb746dffb4f373659d6
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-22 14:15:34 +0200

    Update 10_make_supp_tables.R

--- code/10_make_supp_tables.R
+++ code/10_make_supp_tables.R
@@ -3,7 +3,7 @@ source("01_requirements.R")
 #Make supplementary table of all GB features
 GB_df_desc <- read_tsv("../grambank-analysed/R_grambank/output/GB_wide/parameters_binary.tsv") %>% 
   filter(Binary_Multistate != "Multi") %>% 
-  dplyr::select(`Feature\\_ID` = ID, Name)
+  dplyr::select(`Feature_ID` = ID, Name)
 
 cap <- "Table of Grambank fetures"
 lbl <- "GB_features_table"
@@ -19,10 +19,10 @@ xtable(caption = cap, label = lbl,
 
 
  ##make a table of all reconstructions that the 6 main methods agree on as present
-all_reconstructions <- read_tsv("output/HL_comparison/all_reconstructions.tsv") %>% 
+extra_reconstructions <- read_tsv("output/HL_comparison/extra_predictions.tsv") %>% 
   distinct()
 
-six_out_of_six_agree <- all_reconstructions %>% 
+six_out_of_six_agree <- extra_reconstructions %>% 
   reshape2::melt(id.vars = c("Feature_ID", "Proto-language")) %>% 
   filter(variable != "most_common_prediction") %>% 
   filter(!str_detect(value, "enough")) %>% 
@@ -32,8 +32,9 @@ six_out_of_six_agree <- all_reconstructions %>%
   filter(value == "Present") %>% 
   dplyr::select("Feature_ID", "Proto-language")
 
-all_reconstructions_for_xtable <- all_reconstructions %>%
+extra_reconstructions_for_xtable <- extra_reconstructions %>%
   inner_join(six_out_of_six_agree) %>% 
+#  left_join(GB_df_desc) %>%  View()
   rename(`Feature\\_ID` = Feature_ID,
     `Glottolog Parsimony`= glottolog_parsimony_prediction,
          `Gray parismony -MCCT` = gray_parsimony_prediction,
@@ -41,13 +42,14 @@ all_reconstructions_for_xtable <- all_reconstructions %>%
          `Glottolog ML` =glottolog_ML_prediction,
           `Gray ML -MCCT` = gray_ML_prediction,
           `Gray ML -posteriors` = gray_ML_prediction_posteriors,
-`Most Common`= most_common_prediction) 
+`Most Common`= most_common_prediction) %>% 
+  dplyr::select(-Name)
   
 cap <- "Table showing predictions where the six MP and ML methods agree on presence"
 lbl <- "extra_predictions_table"
 align <- c("p{1.5cm}", "p{1.5cm}","p{2.5cm}", "p{2.5cm}", "p{2.5cm}", "p{2.5cm}", "p{2.5cm}", "p{2.5cm}", "p{2.5cm}", "p{2.5cm}") 
 
-all_reconstructions_for_xtable %>% 
+extra_reconstructions_for_xtable %>% 
 xtable(caption = cap, label = lbl,
        digits = 0, 
        align = align) %>% 
