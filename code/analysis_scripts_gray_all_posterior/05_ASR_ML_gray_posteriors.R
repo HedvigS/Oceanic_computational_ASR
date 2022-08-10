bb2c44fb5432df0297f7fb58363e2132fff3cf7e
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-10 14:51:10 +0200

    Update 05_ASR_ML_gray_posteriors.R

--- code/analysis_scripts_gray_all_posterior/05_ASR_ML_gray_posteriors.R
+++ code/analysis_scripts_gray_all_posterior/05_ASR_ML_gray_posteriors.R
@@ -4,7 +4,7 @@ glottolog_df <- read_tsv("output/processed_data/glottolog_language_table_wide_df
   dplyr::select(Glottocode, Language_level_ID, level, classification, Name)
 
 #reading in GB
-GB_df_desc <-  data.table::fread("../grambank-analysed/R_grambank/output/GB_wide/parameters_binary.tsv",
+GB_df_desc <-  data.table::fread(GB_df_desc_fn,
                                  encoding = 'UTF-8', 
                                  quote = "\"", 
                                  fill = T, 
@@ -15,7 +15,7 @@ GB_df_desc <-  data.table::fread("../grambank-analysed/R_grambank/output/GB_wide
   mutate(Grambank_ID_desc = str_replace_all(Grambank_ID_desc, " ", "_"))
 
 #reading in Grambank
-GB_df_all <- read_tsv("../grambank-analysed/R_grambank/output/GB_wide/GB_wide_binarized.tsv", col_types = cols()) 
+GB_df_all <- read_tsv(GB_binary_fn, col_types = cols()) 
 
 
 #reading in gray et all tree, already subsetted to only Oceanic and with tips renamed to glottocodes. If the tip was associated with a dialect which was individually coded in GB, the tip label is the glottocode for that dialect. If not, it has the language-level parent glottocode of that dialect. We'll be dropping tips with missing data feature-wise, i.e. for each feature not before.
