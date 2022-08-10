2aa3dd5644a0c51a7c7caf4b6f04c5682a1e9433
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-10 14:39:50 +0200

    Update 01_requirements.R

--- code/01_requirements.R
+++ code/01_requirements.R
@@ -215,8 +215,8 @@ HL_findings_sheet_fn <- "output/processed_data/HL_findings/HL_findings_for_compa
 
 HL_findings_sheet_conflicts_fn <- "data/HL_findings_conflicts.csv"
 
-GB_df_desc_fn <- "../grambank-analysed/R_grambank/output/GB_wide/parameters_binary.tsv"
+GB_df_desc_fn <- "output/GB_wide/parameters_binary.tsv"
+
+GB_binary_fn <- "output/GB_wide/GB_wide_binarized.tsv"
 
 glottolog_df_fn <- "output/processed_data/glottolog_language_table_wide_df.tsv"
-
-GB_binary_fn <- "../grambank-analysed/R_grambank/output/GB_wide/GB_wide_binarized.tsv"
