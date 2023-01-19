61083adf4a97d5d294419f670cf68d418843f296
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-01-19 13:50:57 +0100

    Update 01_requirements.R

--- code/01_requirements.R
+++ code/01_requirements.R
@@ -8,8 +8,11 @@ source("fun_def_h_load.R")
 #do a binary install of all of tidyverse specifically
 if(!"tidyverse" %in% rownames(installed.packages())  ){
   install.packages("tidyverse", type="binary", dependencies = T)
-  }
+}
+
+suppressMessages( 
 library("tidyverse", quietly = T, warn.conflicts = F, verbose = F)
+)
 #depdencies for castor
 h_load(c("naturalsort", "RSpectra"))
 
