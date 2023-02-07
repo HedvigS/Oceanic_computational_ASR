b816c4f21b8870d9a2bae69cce8e4c67d6385049
HedvigS <hedvig.skirgard@gmail.com>
2023-02-07 21:54:06 +0100

    Update 09e_extra_predictions.R

--- code/09e_extra_predictions.R
+++ code/09e_extra_predictions.R
@@ -17,14 +17,34 @@ full_df <- read_tsv("output/all_reconstructions_all_methods_long.tsv") %>%
   pivot_wider(names_from = "variable", values_from = "value") %>% 
   filter(is.na(result)) %>% 
   filter(!is.na(`Proto-language`)) %>% 
-  dplyr::select(-result)# %>% 
-#  pivot_wider(names_from = "method", values_from = "prediction") %>% 
- # dplyr::select(-HL_HL)
+  dplyr::select(-result)
 
-full_df %>% 
+df_agreement <- full_df %>% 
+  filter(!str_detect(method, "HL")) %>% 
   group_by(Feature_ID, `Proto-language`, prediction) %>% 
   summarise(n = n(), .groups = "drop") %>% 
+  filter(prediction == "Present") %>% 
   filter(n == 7) %>% 
-  left_join(GB_ID_desc, by = "Feature_ID") %>%
+  dplyr::select(-n)
+
+df_agreement_without_common <- full_df %>% 
+  filter(!str_detect(method, "common")) %>% 
+  filter(!str_detect(method, "HL")) %>% 
+  group_by(Feature_ID, `Proto-language`, prediction) %>% 
+  summarise(n = n(), .groups = "drop") %>% 
+  filter(prediction == "Present") %>% 
+  filter(n == 6) %>% 
+  dplyr::select(-n)
+
+df_agreement_without_common %>% 
+  anti_join(df_agreement) %>% 
+left_join(GB_ID_desc, by = "Feature_ID") %>% View()
+
+
+
+  filter(n == 7) %>% 
+  left_join(GB_ID_desc, by = "Feature_ID") %>% View()
   write_tsv("output/HL_comparison/extra_predictions_all_agree.tsv")
 
+
+  x <- read_tsv(file = "output/all_reconstructions_all_methods_wide_easy.tsv", na = "")
