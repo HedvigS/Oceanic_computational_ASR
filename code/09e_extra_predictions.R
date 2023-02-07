9d299c15263a3e40e956396c390b03fa2017d8c3
HedvigS <hedvig.skirgard@gmail.com>
2023-02-07 21:35:45 +0100

    Update 09e_extra_predictions.R

--- code/09e_extra_predictions.R
+++ code/09e_extra_predictions.R
@@ -9,62 +9,22 @@ if(!dir.exists(OUTPUT_DIR)){
   dir.create(OUTPUT_DIR)}
 
 #HL sheets
-HL_findings_sheet <- read_tsv("output/processed_data/HL_findings/HL_findings_for_comparison.tsv") %>% 
-  filter(!is.na(Prediction)) %>% 
-  distinct(Feature_ID, `Proto-language`)
-
-HL_findings_sheet_conflicts <- read_csv(HL_findings_sheet_conflicts_fn) %>% 
-  rename(Prediction = Value) %>% 
-  filter(!is.na(Prediction)) %>% 
-  distinct(Feature_ID, `Proto-language`)
-
-HL_findings_sheets <- full_join(HL_findings_sheet, HL_findings_sheet_conflicts, by = c("Proto-language", "Feature_ID"))
-
-#the results per method
-most_common_df <- read_tsv("output/HL_comparison/most_common_reconstructions.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", "most_common_prediction")
-
-parsimony_glottolog_df <- read_tsv("output/glottolog-tree/parsimony/all_reconstructions.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", "glottolog_parsimony_prediction")
-
-parsimony_gray_mcct_df <- read_tsv("output/gray_et_al_2009/parsimony/mcct/all_reconstructions.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", "gray_parsimony_prediction")
-
-parsimon_gray_posteriors_df <- read_tsv("output/gray_et_al_2009/parsimony/all_reconstructions_posteriors_aggregated.tsv")   %>% 
-  dplyr::select("Feature_ID", "Proto-language", "gray_parsimony_prediction_posteriors" = gray_parsimony_prediction)
-  
-ML_glottolog_df <- read_tsv("output/glottolog-tree/ML/all_reconstructions.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", "glottolog_ML_prediction")
-
-ML_gray_mcct <- read_tsv("output/gray_et_al_2009/ML/mcct/all_reconstructions.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", "gray_ML_prediction")
-
-ML_gray_posteriors_df <- read_tsv("output/gray_et_al_2009/ML/all_reconstructions_posteriors_aggregated.tsv") %>% 
-  dplyr::select("Feature_ID", "Proto-language", gray_ML_prediction_posteriors = gray_ML_prediction)
-
-#combining all
-full_df <- parsimony_glottolog_df %>% 
-  full_join(parsimony_gray_mcct_df) %>% 
-  full_join(parsimon_gray_posteriors_df) %>% 
-  full_join(ML_glottolog_df) %>% 
-  full_join(ML_gray_mcct) %>% 
-  full_join(ML_gray_posteriors_df) %>% 
-  full_join(most_common_df)
-
-#full_df %>% 
-#  anti_join(HL_findings_sheets, by = c("Feature_ID", "Proto-language") ) %>% 
-#  reshape2::melt(id.vars = c("Feature_ID", "Proto-language")) %>% 
-#  filter(!str_detect(value, "not")) %>% 
-#  filter(!str_detect(value, "alf")) %>% 
-#  distinct(Feature_ID, `Proto-language`, .keep_all = T) %>% View()
+full_df <- read_tsv("output/all_reconstructions_all_methods_long.tsv") %>% 
+  filter(variable == "result"|variable == "prediction") %>% 
+  filter(is.na(conflict)) %>% 
+  dplyr::select(-conflict) %>% 
+  unite(method, tree_type, sep = "_", col = "method") %>% 
+  pivot_wider(names_from = "variable", values_from = "value") %>% 
+  filter(is.na(result)) %>% 
+  filter(!is.na(`Proto-language`)) %>% 
+  dplyr::select(-result)# %>% 
+#  pivot_wider(names_from = "method", values_from = "prediction") %>% 
+ # dplyr::select(-HL_HL)
 
 full_df %>% 
-write_tsv("output/HL_comparison/all_reconstructions.tsv", na = "")
+  group_by(Feature_ID, `Proto-language`, prediction) %>% 
+  summarise(n = n(), .groups = "drop") %>% 
+  filter(n == 7) %>% 
+  left_join(GB_ID_desc, by = "Feature_ID") %>%
+  write_tsv("output/HL_comparison/extra_predictions_all_agree.tsv")
 
-full_df <- full_df %>% 
-anti_join(HL_findings_sheets, by = c("Feature_ID", "Proto-language") ) 
-  
-
-full_df %>% 
-  left_join(GB_ID_desc) %>%
-  write_tsv("output/HL_comparison/extra_predictions.tsv")
