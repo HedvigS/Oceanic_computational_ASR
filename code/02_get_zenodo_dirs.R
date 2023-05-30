cb2001fe5b83d2c4f6067c7eb2d406af18b2b8ff
HedvigS <hedvig.skirgard@gmail.com>
2023-05-30 22:44:54 +0200

    Update 02_get_zenodo_dirs.R

--- code/02_get_zenodo_dirs.R
+++ code/02_get_zenodo_dirs.R
@@ -3,19 +3,20 @@
 source("01_requirements.R")
 source("fun_def_get_zenodo.R")
 
-if(!dir.exists("../grambank_analysed/")){
 
-get_zenodo_dir(url = "https://zenodo.org/record/7740822/files/grambank/grambank-analysed-v1.0.zip", exdir = "../grambank_analysed/")
+if(!file.exists("../grambank-analysed/R_grambank/coverage_bar_plots.R")){
+
+get_zenodo_dir(url = "https://zenodo.org/record/7740822/files/grambank/grambank-analysed-v1.0.zip", exdir = "../grambank-analysed/")
 }
 
-if(!file.exists("../grambank_analysed/grambank/cldf/codes.csv")){
+if(!file.exists("../grambank-analysed/grambank/cldf/codes.csv")){
   
-get_zenodo_dir(url = "https://zenodo.org/record/7740140/files/grambank/grambank-v1.0.zip", exdir = "../grambank_analysed/grambank/")
+get_zenodo_dir(url = "https://zenodo.org/record/7740140/files/grambank/grambank-v1.0.zip", exdir = "../grambank-analysed/grambank/")
 }
 
-if(!file.exists("../grambank_analysed/glottolog-cldf/cldf/codes.csv")){
+if(!file.exists("../grambank-analysed/glottolog-cldf/cldf/codes.csv")){
   
-get_zenodo_dir(url ="https://zenodo.org/record/5772649/files/glottolog/glottolog-cldf-v4.5.zip", exdir = "../grambank_analysed/glottolog-cldf/")
+get_zenodo_dir(url ="https://zenodo.org/record/5772649/files/glottolog/glottolog-cldf-v4.5.zip", exdir = "../grambank-analysed/glottolog-cldf/")
 }
 
 
