829fb40d157e57930969098d37b85a6093c5bde8
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-06-09 16:16:26 +0200

    Update 03_tree_heatmap.R

--- code/03_tree_heatmap.R
+++ code/03_tree_heatmap.R
@@ -37,5 +37,5 @@ phytools::phylo.heatmap(tree = gray_tree,X = GB_df_all,
                         labels = F,
                         fsize = 0.5,
                         legend = F, 
-                        offset = 0.0001, colors = c( "#ffffbf", colours_binary[1]))
+                        offset = 0.0001, colors = c( "#ffffbf", colours_binary[2]))
 x <- dev.off()
