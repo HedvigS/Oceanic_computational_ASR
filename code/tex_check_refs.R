5ed755784fe02db11f95244c57ddd3b3835e9e19
HedvigS <hedvig.skirgard@gmail.com>
2023-09-21 13:23:51 +0200

    Update tex_check_refs.R

--- code/tex_check_refs.R
+++ code/tex_check_refs.R
@@ -1,20 +1,22 @@
 source("01_requirements.R")
 
+#this script takes a tex file and filters to all the \cite commands 
+
 tex <- readLines("../tex/ASR_Oceanic.tex") %>% 
   as.data.frame() %>% 
   rename(text = ".") %>% 
-  mutate(text = str_replace(text, pattern = "\\\\%", replacement = "\\€")) %>% #remove commented out
-  mutate(text = str_replace(text, pattern = "%.*",replacement =  ""))  %>% 
-  mutate(text = str_replace(text, pattern = "\\€", replacement = "\\\\%")) %>%
-  mutate(text = str_split(text, " ")) %>% 
-    unnest(text) %>% 
-  filter(str_detect(text, "\\\\cite")) %>%
-  mutate(text = str_extract(text, "\\\\cite.*\\}")) %>% 
-  mutate(text = str_replace(text, "\\[.*\\]", "")) %>% 
-  mutate(text = str_replace(text, "\\\\cite.*\\{", "")) %>% 
-  mutate(text = str_replace(text, "\\}", ""))
+  mutate(text = str_replace(text, pattern = "\\\\%", replacement = "\\€")) %>% #replace "actual" percentage marks with something else in order to use the comment percent marks 
+  mutate(text = str_replace(text, pattern = "%.*",replacement =  ""))  %>% #replace any content that starts with comment out percent mark with nothing, i.e. remove them
+  mutate(text = str_replace(text, pattern = "\\€", replacement = "\\\\%")) %>% #put the actual percent marks back in
+  mutate(text = str_split(text, " ")) %>% #split at spaces
+    unnest(text) %>% #unnest, i.e. each "word" (thing between spaces) on one line
+  filter(str_detect(text, "\\\\cite")) %>% #filter to only those with the cite command
+  mutate(text = str_extract(text, "\\\\cite.*\\}")) %>% #extract so that irrelevant things are dropped
+  mutate(text = str_replace(text, "\\[.*\\]", "")) %>% #remove page number arguments
+  mutate(text = str_replace(text, "\\\\cite.*\\{", "")) %>% #remove cite command
+  mutate(text = str_replace(text, "\\}", "")) #remove last curly bracket
 
-cat(paste0("You cited a paper ", tex %>% nrow(), " times.\n"))
+cat(paste0("You used ", tex %>% nrow(), " citations.\n"))
 cat(paste0("You cited ", distinct(tex) %>% nrow(), " unique papers.\n"))
 
 cat(paste0("Your top five most cited references were: "))
